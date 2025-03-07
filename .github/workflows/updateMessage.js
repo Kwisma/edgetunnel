c9294b3f2700d45937390a3c42a8daae2b726a74
sameu <77215384+Kwisma@users.noreply.github.com>
2025-03-07 21:52:52 +0800

    Create updateMessage.js

---
+++ .github/workflows/updateMessage.js
@@ -0,0 +1,23 @@
+name: updateMessage
+
+on:
+    workflow_dispatch:
+    schedule:
+      - cron: '0 * * * *'
+
+jobs:
+  obfuscate:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+
+      - name: Use Node.js
+        uses: actions/setup-node@v4
+        with:
+          node-version: node
+
+      - name: Install dependencies
+        run: npm install
+
+      - name: Obfuscate code
+        run: node tg.js
